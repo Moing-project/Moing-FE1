cb8b6dd97c96c790c663f27e314b41cf68bae5f4
jwc406 <77850103+jwc406@users.noreply.github.com>
2023-08-05 11:26:35 +0900

    라우팅 연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,48 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Layout from "./LayoutComponents/Layout";
+import Rending from "./pageComponents/Rending";
+import Main from "./pageComponents/Main";
+import Projects from "./pageComponents/Projects";
+import MoingLayout from "./LayoutComponents/MoingLayout";
+import Kanban from "./pageComponents/Kanban";
+import Issue from "./pageComponents/Issue";
+import IssueDetail from "./pageComponents/IssueDetail";
+import MoingSpace from "./pageComponents/MoingSpace";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    // <Routes>
+    //   <Route path="/" element={<Layout />}>
+    //     <Route index element={<Rending />} />
+    //     <Route path="main" element={<Main />} />
+    //     <Route path="projects" element={<Projects />} />
+    //   </Route>
+    //   {/* MoingSpace 컴포넌트를 루트로 설정 */}
+    //   <Route path="/" element={<MoingLayout />}>
+    //     {/* 이제 MoingSpace 컴포넌트 내부에 중첩된 라우팅을 구성합니다. */}
+    //     <Route path="moingSpace" element={<MoingSpace />} />
+    //     <Route path="kanban" element={<Kanban />} />
+    //     <Route path="issue" element={<Issue />}>
+    //       <Route path="detail" element={<IssueDetail />} />
+    //     </Route>
+    //   </Route>
+    // </Routes>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Rending />} />
+        <Route path="main" element={<Main />} />
+        <Route path="projects" element={<Projects />} />
+      </Route>
+      <Route path="/moingSpace/*" element={<MoingLayout />}>
+        <Route index element={<MoingSpace />} />
+        <Route path="kanban" element={<Kanban />} />
+        <Route path="issue" element={<Issue />}>
+          <Route path="detail" element={<IssueDetail />} />
+        </Route>
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pageComponents/MoingSpace.tsx
@@ -0,0 +1,3 @@
+export default function MoingSpace() {
+  return <div>모잉 스페이스 메인입니다</div>;
+}
